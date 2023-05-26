1eaa6753c4a13d46fc3cdf9a46c35f12feb21d11
theripper93 <tsplab@gmail.com>
2023-05-26 17:19:08 +0200

    a

--- scripts/handlers/soundHandler.js
+++ scripts/handlers/soundHandler.js
@@ -2,8 +2,13 @@ export class SoundHandler{
     static isAudible(wrapped, ...args) {
         const result = wrapped(...args);
         const currentElevation = WallHeight.currentTokenElevation
-        if(currentElevation === null || currentElevation === undefined) return result;
-        const inRange = CONFIG.Levels.helpers.inRange(this.document, currentElevation);
+        if (currentElevation === null || currentElevation === undefined) return result;
+        let inRange;
+        if (game.Levels3DPreview?._active) {
+            inRange = canvas.tokens.placeables.some((t) => CONFIG.Levels.helpers.inDistance(t, this, this.radius)) ;
+        } else {
+            inRange = CONFIG.Levels.helpers.inRange(this.document, currentElevation);
+        }
         return result && inRange;
     }
 }
