da4e0f45db3c8b449db9f3ea3c0f21852d60bc5e
theripper93 <tsplab@gmail.com>
2025-05-20 15:16:32 +0200

    a

--- scripts/handlers/regionHandler.js
+++ scripts/handlers/regionHandler.js
@@ -2,31 +2,31 @@ import { DrawingHandler } from "./drawingHandler.js";
 
 export class RegionHandler {
     static elevator(region, event, elevatorData) {
-        if(game.user !== event.user) return;
-        const {top, bottom, tokenDocument, elevation} = this.getRegionEventData(region, event);
+        if (game.user !== event.user) return;
+        const { top, bottom, tokenDocument, elevation, movement } = this.getRegionEventData(region, event);
         if (elevation > top || elevation < bottom) return;
         DrawingHandler.renderElevatorDalog(elevatorData);
     }
-    
+
     static stair(region, event) {
-        if(game.user !== event.user) return;
-        const {top, bottom, tokenDocument, elevation} = this.getRegionEventData(region, event);
+        if (game.user !== event.user) return;
+        const { top, bottom, tokenDocument, elevation, movement } = this.getRegionEventData(region, event);
         if (elevation !== bottom && elevation !== top) return;
-        RegionHandler.updateMovement(tokenDocument, elevation === top ? bottom : top);
+        RegionHandler.updateMovement(tokenDocument, elevation === top ? bottom : top, movement);
     }
 
     static stairDown(region, event) {
-        if(game.user !== event.user) return;
-        const {top, bottom, tokenDocument, elevation} = this.getRegionEventData(region, event);
+        if (game.user !== event.user) return;
+        const { top, bottom, tokenDocument, elevation, movement } = this.getRegionEventData(region, event);
         if (elevation > top || elevation <= bottom) return;
-        RegionHandler.updateMovement(tokenDocument, bottom);
+        RegionHandler.updateMovement(tokenDocument, bottom, movement);
     }
 
     static stairUp(region, event) {
-        if(game.user !== event.user) return;
-        const {top, bottom, tokenDocument, elevation} = this.getRegionEventData(region, event);
+        if (game.user !== event.user) return;
+        const { top, bottom, tokenDocument, elevation, movement } = this.getRegionEventData(region, event);
         if (elevation < bottom || elevation >= top) return;
-        RegionHandler.updateMovement(tokenDocument, top);
+        RegionHandler.updateMovement(tokenDocument, top, movement);
     }
 
     static getRegionEventData(region, event) {
@@ -35,18 +35,25 @@ export class RegionHandler {
             bottom: region.elevation.bottom,
             tokenDocument: event.data.token,
             elevation: event.data.token.elevation,
+            movement: event.data.movement,
         }
     }
 
-    static updateMovement(tokenDocument, elevation){
-        const position = tokenDocument.getSnappedPosition({...tokenDocument.movement.destination, elevation, action: "displace"});
+    static async updateMovement(tokenDocument, elevation, movement) {
         tokenDocument.stopMovement();
-        tokenDocument.move([{...position, action: "displace"}], {...tokenDocument.movement});
+        if (tokenDocument.rendered) await tokenDocument.object.movementAnimationPromise;
+        const adjustedWaypoints = movement.pending.waypoints.filter(w => !w.intermediate).map(w => ({ ...w, elevation, action: "displace" }));
+        await tokenDocument.move(adjustedWaypoints, {
+            ...movement.updateOptions,
+            constrainOptions: movement.constrainOptions,
+            autoRotate: movement.autoRotate,
+            showRuler: movement.showRuler
+        });
     }
 
-    static async waitForAnimation(tokenDocument, fn){
+    static async waitForAnimation(tokenDocument, fn) {
         const object = tokenDocument.object;
-        const promises = Array.from(object.animationContexts.values()).map(a=>[a.promise, ...a.chain.map(c=>c.promise)]).flat();
+        const promises = Array.from(object.animationContexts.values()).map(a => [a.promise, ...a.chain.map(c => c.promise)]).flat();
         await Promise.all(promises);
         fn();
     }
